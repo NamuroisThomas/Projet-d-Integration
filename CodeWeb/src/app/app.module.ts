64c16b864a34c9aa7c7d05552370cebd0de7b079
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-09-24 12:28:45 +0200

    creation des differents component de base

--- CodeWeb/src/app/app.module.ts
+++ CodeWeb/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { DemandeAideComponent } from './demande-aide/demande-aide.component';
+import { ChatComponent } from './chat/chat.component';
+import { ListeDemandeComponent } from './liste-demande/liste-demande.component';
+import { EditProfilComponent } from './edit-profil/edit-profil.component';
+import { AvisReportComponent } from './avis-report/avis-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    DemandeAideComponent,
+    ChatComponent,
+    ListeDemandeComponent,
+    EditProfilComponent,
+    AvisReportComponent
   ],
   imports: [
     BrowserModule,
---
+++ CodeWeb/src/app/demande-aide/demande-aide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demande-aide',
+  templateUrl: './demande-aide.component.html',
+  styleUrls: ['./demande-aide.component.scss']
+})
+export class DemandeAideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
