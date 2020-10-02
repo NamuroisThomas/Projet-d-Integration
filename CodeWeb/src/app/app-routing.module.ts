2174795080d96a84e1ceff79488f44de8b7d8dfc
FrancoisT97 <56149458+FrancoisT97@users.noreply.github.com>
2020-10-02 15:40:39 +0200

    correction bug routing

--- CodeWeb/src/app/app-routing.module.ts
+++ CodeWeb/src/app/app-routing.module.ts
@@ -4,14 +4,20 @@ import {DemandeAideComponent} from './demande-aide/demande-aide.component';
 import {InscriptionComponent} from './inscription/inscription.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {HomeComponent} from './home/home.component';
-//https://angular.io/guide/cheatsheet
+
+
 
 const appRoutes: Routes = [
-  {path: 'inscription', component: InscriptionComponent},
-  {path: 'demandeAide', component: DemandeAideComponent},
-  {path: '', component: HomeComponent},
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
+  {
+    path: '', component: HomeComponent,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'demandeAide', component: DemandeAideComponent},
+      {path: 'inscription', component: InscriptionComponent},
+      {path: 'not-found', component: PageNotFoundComponent},
+      {path: '**', redirectTo: 'home'}
+    ]
+  }
 ];
 
 @NgModule({
