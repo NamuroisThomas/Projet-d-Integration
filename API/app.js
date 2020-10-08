656a6c6316ddf66000f855e38c7d5f2643e561cb
GGEphec <he201732@students.ephec.be>
2020-10-08 10:26:27 +0200

    API/test ok

--- API/app.js
+++ API/app.js
@@ -2,11 +2,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+app.use(function (req, res, next) {
+    res.locals.connection = mysql.createConnection({
+        //host     : '62.210.130.145',
+        host     : 'localhost',
+        //user     : 'projetI',
+        user       : 'root',
+        //password : 'Integration7',
+        password   : '',
+        //database : 'NeedHelpV2'
+        database : 'needhelp'
+    });
+    res.locals.connection.connect(function(error){
+        if(error){
+            //res.render('error', {message:"Impossible de se connecter à la base de données"});
+        }
+        else {
+            //res.locals.connection.connect();
+            next();
+        }
+    });
+});
+
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- API/routes/index.js
+++ API/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Vérification de l'identifiant et du mot de passe dans la base de donnée
+//Retourne les droits de la personne s'étant connectée
+router.get('/test', function(req,res,next){
+
+        console.log('API test');
+        res.locals.connection.query('SELECT * FROM categories', function(error, results, fields) {
+            if (error!=null) {
+                res.redirect(529, '/error');
+                console.log("erreur query");
+            }
+            else {
+                res.send({"status": 200, "error": null, "response": results});
+                console.log("query OK");
+
+            }
+        });
+});
+
+
+
 module.exports = router;
