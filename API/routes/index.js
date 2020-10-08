a6f377985b6c514196fea097d43a7176304ec314
GGEphec <he201732@students.ephec.be>
2020-10-08 11:36:56 +0200

    debut API

--- API/routes/index.js
+++ API/routes/index.js
@@ -3,8 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/demande', function(req,res,next){
+
+    res.locals.connection.query('SELECT * FROM demandes', function(error, results, fields) {
+        if (error!=null) {
+            res.redirect(529, '/error');
+            console.log("erreur query");
+        }
+        else {
+            res.send({"status": 200, "error": null, "response": results});
+            console.log("query OK");
+        }
+    });
+});
+
 
 //Vérification de l'identifiant et du mot de passe dans la base de donnée
 //Retourne les droits de la personne s'étant connectée
@@ -24,6 +39,64 @@ router.get('/test', function(req,res,next){
         });
 });
 
+router.get('/idDemande', function(req,res,next){
+
+    var demande_id = req.query.idDemande;
+    console.log(demande_id);
+
+    res.locals.connection.query('SELECT idDemande FROM demandes  WHERE idDemande=?',[demande_id], function(error, results, fields) {
+        if (error!=null) {
+            res.redirect(529, '/error');
+            console.log("erreur query");
+        }
+        else {
+            res.send({"status": 200, "error": null, "response": results});
+            console.log("query OK");
+        }
+    });
+});
+
+
+
+
+
+router.get('/nomUtilisateur', function(req,res,next){
+
+    var nom_utilisateur = req.query.nomUtilisateur;
+
+
+    res.locals.connection.query('SELECT * FROM utilisateurs  WHERE =?',[nom_utilisateur], function(error, results, fields) {
+        if (error!=null) {
+            res.redirect(529, '/error');
+            console.log("erreur query");
+        }
+        else {
+            res.send({"status": 200, "error": null, "response": results});
+            console.log("query OK");
+
+        }
+    });
+});
+
+
+router.get('/mailExist', function(req,res,next){
+
+    var mail_utilisateur = req.query.mailUtilisateur
+
+    //if ()
+
+    res.locals.connection.query('SELECT * FROM utiisateurs where mailUtilisateur=?', [] , function(error, results, fields) {
+        if (error!=null) {
+            res.redirect(529, '/error');
+            console.log("erreur query");
+        }
+        else {
+            res.send({"status": 200, "error": null, "response": results});
+            console.log("query OK");
+
+        }
+    });
+});
 
 
 module.exports = router;
