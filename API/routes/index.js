372e8a6e5b5e5377c0c3e2725cc05b1e8fb8f3c0
GGEphec <he201732@students.ephec.be>
2020-10-14 17:09:49 +0200

    API mise a jour commentaires Rapport réunion

--- API/routes/index.js
+++ API/routes/index.js
@@ -7,10 +7,8 @@ router.get('/', function(req, res, next) {
 });
 
 
-//Vérification de l'identifiant et du mot de passe dans la base de donnée
-//Retourne les droits de la personne s'étant connectée
+//Test de l'API retoure toutes les catégories
 router.get('/test', function(req,res,next){
-
         console.log('API test');
         res.locals.connection.query('SELECT * FROM categories', function(error, results, fields) {
             if (error!=null) {
@@ -20,20 +18,17 @@ router.get('/test', function(req,res,next){
             else {
                 res.send({"status": 200, "error": null, "response": results});
                 console.log("query OK");
-
             }
         });
 });
 
-// à tester 
-// récupération des demandes par id si spécifié, sinon revoie toutes les demandes  
+
+//récupération des demandes par id si spécifié, sinon revoie toutes les demandes
 router.get('/demande', function(req,res,next){
-	
 	var demande_id = req.query.idDemande;
-		console.log(demande_id);
-	
+    console.log('GET demande');
 	if (demande_id != undefined){
-		
+	    console.log('GET demande by id '+demande_id);
 		res.locals.connection.query('SELECT * FROM demandes  WHERE idDemande=?',[demande_id], function(error, results, fields) {
 			if (error!=null) {
 				res.redirect(529, '/error');
@@ -45,8 +40,8 @@ router.get('/demande', function(req,res,next){
 			}
 		});
 	} else {
-		
-		res.locals.connection.query('SELECT * FROM demandes, function(error, results, fields) {
+	    console.log('GET demande all');
+		res.locals.connection.query('SELECT * FROM demandes', function(error, results, fields) {
 			if (error!=null) {
 				res.redirect(529, '/error');
 				console.log("erreur query");
@@ -59,13 +54,12 @@ router.get('/demande', function(req,res,next){
 	}
 });
 
-// à tester 
-// vérification de l'utilisateur (nom, prenom) et de son mot de passe sur base de l'adresse mail
-router.get('/utilisateur', function(req,res,next){
 
-    var mailUtilisateur = req.query.mailUtilisateur;
-	
-	res.locals.connection.query('SELECT CONCAT(nomUtilisateur, ' ', prenomUtilisateur) as utilisateur , mdpUtilisateur as mdp FROM utilisateurs where mailUtilisateur=?',[mailUtilisateur], function(error, results, fields) {
+//Vérification de l'utilisateur (nom, prenom) et de son mot de passe sur base de l'adresse mail
+router.get('/utilisateur', function(req,res,next){
+    var mailUtilisateur = req.query.mail;
+    console.log('GET user + MDP by email '+mailUtilisateur);
+	res.locals.connection.query('SELECT CONCAT(nomUtilisateur, " ", prenomUtilisateur) as utilisateur , mdpUtilisateur as mdp FROM utilisateurs where mailUtilisateur=?',[mailUtilisateur], function(error, results, fields) {
 		if (error!=null) {
 			res.redirect(529, '/error');
 			console.log("erreur query");
@@ -73,41 +67,16 @@ router.get('/utilisateur', function(req,res,next){
 		else {
 			res.send({"status": 200, "error": null, "response": results});
 			console.log("query OK");
-
 		}
-		
 	});
 });
 
-
-/*
-router.get('/nomUtilisateur', function(req,res,next){
-
-    var nom_utilisateur = req.query.nomUtilisateur;
-
-
-    res.locals.connection.query('SELECT * FROM utilisateurs  WHERE =?',[nom_utilisateur], function(error, results, fields) {
-        if (error!=null) {
-            res.redirect(529, '/error');
-            console.log("erreur query");
-        }
-        else {
-            res.send({"status": 200, "error": null, "response": results});
-            console.log("query OK");
-
-        }
-    });
-});
-
-
-
+//TODO a verif
+//Verif si mail est deja pris
 router.get('/mailExist', function(req,res,next){
-
-    var mail_utilisateur = req.query.mailUtilisateur
-
-    //if ()
-
-    res.locals.connection.query('SELECT * FROM utiisateurs where mailUtilisateur=?', [] , function(error, results, fields) {
+    var mailUtilisateur = req.query.mail;
+    console.log('GET mail exist');
+    res.locals.connection.query('SELECT * FROM utiisateurs where mailUtilisateur=?',[mailUtilisateur] , function(error, results, fields) {
         if (error!=null) {
             res.redirect(529, '/error');
             console.log("erreur query");
@@ -115,44 +84,52 @@ router.get('/mailExist', function(req,res,next){
         else {
             res.send({"status": 200, "error": null, "response": results});
             console.log("query OK");
-
         }
     });
 });
-*/
 
-// à tester 
-// récupération des données du formulaire inscription et ajout dans la base de donnée 
-router.post('/formInscription', function (req, res, next) {
 
-    console.log(req);
-
-        res.locals.connection.query('INSERT INTO utilisateurs (nomUtilisateur, prenomUtilisateur, mailUtilisateur, telUtilisateur, mdpUtilisateur, idStatus, descriptionUtilisateur, avertissementUtiisateur) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[req.body.formUtilisateurNom, req.body.formUtilisateurPrenom, req.body.formUtilisateurMail, req.body.formUtilisateurTel, req.body.formUtilisateurMdp, 4, " ", 0], function (error, results, fields) {
-            if (error!=null) {
-                res.redirect(529, '/error');
-            }
-            else {
-                console.log("utilisateur ajouté");
-                res.redirect(req.headers.referer);
-            } //TODO pas juste changer la redirection
-        });
+// TODO à tester
+// TODO ajouter verif mdp egaux et mail (front end)
+// récupération des données du formulaire inscription et ajout dans la base de données
+router.post('/inscription', function (req, res, next) {
+    //console.log(req.body);
+    console.log('POST inscription');
+    res.locals.connection.query('INSERT INTO utilisateurs (nomUtilisateur, prenomUtilisateur, mailUtilisateur, telUtilisateur, mdpUtilisateur) VALUES (?, ?, ?, ?, ?)',[req.body.formUtilisateurNom, req.body.formUtilisateurPrenom, req.body.formUtilisateurMail, req.body.formUtilisateurTel, req.body.formUtilisateurMdp], function (error, results, fields) {
+        if (error!=null) {
+            res.redirect(529, '/error');
+        }
+        else {
+            console.log("utilisateur ajouté");
+            res.send({"status":201, "error": null, "response":results});
+        }
+    });
 });
 
 
-/*
+// TODO à tester
+//récupération des données du formulaire demandes et ajout dans la base de données
 router.post('/formDemande', function (req, res, next) {
-
-    console.log(req);
-
-        res.locals.connection.query('INSERT INTO demandes (nomUtilisateur, prenomUtilisateur, mailUtilisateur, telUtilisateur, mdpUtilisateur, idStatus, descriptionUtilisateur, avertissementUtiisateur) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',[req.body.formUtilisateurNom, req.body.formUtilisateurPrenom, req.body.formUtilisateurMail, req.body.formUtilisateurTel, req.body.formUtilisateurMdp, 4, " ", 0],  function (error, results, fields) {
-            if (error!=null) {
-                res.redirect(529, '/error');
-            }
-            else {
-                console.log("utilisateur ajouté");
-                res.redirect(req.headers.referer);
-            } //TODO pas juste changer la redirection
-        });
+    //console.log(req.body);
+    console.log('POST demande');
+    res.locals.connection.query('INSERT INTO demandes (titreDemande, descriptionDemande, dateDemande, idUtilisateur, idCategorie, defraimentDemande, idCodePostal) VALUES (?, ?, ?, ?, ?, ?, ?)',[req.body.formDemandeTitre, req.body.formDemandeDescription, req.body.formDemandeDate, req.body.formDemandeIdUtilisateur, req.body.formDemandeIdCategorie, req.body.formDemandeDefraiement, req.body.formDemandeIdCodePostal],  function (error, results, fields) {
+        if (error!=null) {
+            res.redirect(529, '/error');
+        }
+        else {
+            console.log("utilisateur ajouté");
+            res.send({"status":201, "error": null, "response":results});
+        }
+    });
 });
-*/
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
