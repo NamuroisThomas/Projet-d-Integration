fe6e69130c862a7ac59054a86c5fc41dd8c55946
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-10-21 16:42:31 +0200

    Connexion a la DB via script php part2

---
+++ applicationMobile/app/src/main/java/com/example/needhelp/modele/AccesDistant.java
@@ -0,0 +1,5 @@
+package com.example.needhelp.modele;
+
+public class AccesDistant {
+
+}
--- applicationMobile/app/src/main/java/com/example/needhelp/outils/AccessHTTP.java
+++ applicationMobile/app/src/main/java/com/example/needhelp/outils/AccessHTTP.java
@@ -1,4 +1,75 @@
 package com.example.needhelp.outils;
 
-public class AccessHTTP {
+import android.os.AsyncTask;
+import android.provider.Settings;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Entity;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+public class AccessHTTP extends AsyncTask<String,Integer,Long> {
+
+    private ArrayList<NameValuePair> parametres;
+    private String ret = null;
+    private AsyncResponse delegate = null;
+
+    /**
+     * Constructeur
+     */
+    public AccessHTTP(){
+        parametres = new ArrayList<NameValuePair>();
+    }
+
+    /**
+     * Ajout d'un parametre post
+     * @param nom
+     * @param valeur
+     */
+    public void addParam(String nom, String valeur){
+        parametres.add(new BasicNameValuePair(nom,valeur));
+    }
+    @Override
+    /**
+     * Connexion en tache de fond dans un thread separe
+     * @param strings
+     * @return
+     */
+    protected Long doInBackground(String... strings) {
+        HttpClient cnxHttp = new DefaultHttpClient();
+        HttpPost paramCnx = new HttpPost(strings[0]);
+
+        try {
+            // encodage des paramètres
+            paramCnx.setEntity(new UrlEncodedFormEntity(parametres));
+            // connexion et envoi des parametres, attente de reponse
+            HttpResponse response = cnxHttp.execute(paramCnx);
+            // transformation de la reponse
+            ret = EntityUtils.toString(response.getEntity());
+
+        } catch (UnsupportedEncodingException e) {
+            Log.d("Erreur Encodage", "**************" + e.toString());
+        } catch (ClientProtocolException e) {
+            Log.d("Erreur protocole", "**************" + e.toString());
+        } catch (IOException e) {
+            Log.d("Erreur I/O", "**************" + e.toString());
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Long result) {
+        delegate.processFinish(ret.toString());
+    }
 }
