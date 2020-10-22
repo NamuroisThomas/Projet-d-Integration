032278ed6894146434e4bed4c8304997d122e5dd
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-10-22 10:55:06 +0200

    inscription complet/fini

--- CodeWeb/src/app/inscription/inscription.component.ts
+++ CodeWeb/src/app/inscription/inscription.component.ts
@@ -15,10 +15,8 @@ export class InscriptionComponent implements OnInit {
   ngOnInit(): void {
   }
   submit(data){
-    this.http.post('http://62.210.130.145:3000/inscription', data)
-      .subscribe((result) =>
-        console.warn('result', result)
-      );
+    this.http.post('http://62.210.130.145:3000/inscription', data);
+    location.reload();
   }
 }
 
