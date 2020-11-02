0c2c3060b0a0477e92cbe8f402fbdd27fc5816e0
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-11-02 17:53:25 +0100

    avancement log in + ajout authguard component

--- CodeWeb/src/app/edit-profil/edit-profil.component.ts
+++ CodeWeb/src/app/edit-profil/edit-profil.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { isNull } from 'util';
 
 @Component({
   selector: 'app-edit-profil',
   templateUrl: './edit-profil.component.html',
   styleUrls: ['./edit-profil.component.scss']
 })
-export class EditProfilComponent implements OnInit {
 
-  constructor() { }
+@Injectable()
 
-  ngOnInit(): void {
+export class EditProfilComponent implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    // Récupération de l'utilisateur connecté
+    const isLoggedIn = !isNull(localStorage.getItem('user'));
+
+    if (!isLoggedIn) {
+      // Si pas d'utilisateur connecté : redirection vers la page de home
+      console.log('Vous n\'êtes pas connectés');
+      this.router.navigate(['/home']);
+    }
+    return isLoggedIn;
   }
-
 }
+
+
+
+/*
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { isNull } from 'util';
+
+
+export class AuthGuard implements CanActivate {
+
+constructor(
+
+) { }
+
+canActivate(
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    // Récupération de l'utilisateur connecté
+    const isLoggedIn = !isNull(localStorage.getItem('user'));
+
+    if (!isLoggedIn) {
+      // Si pas d'utilisateur connecté : redirection vers la page de login
+      console.log('Vous n\'êtes pas connectés');
+      this.router.navigate(['/login']);
+    }
+    return isLoggedIn;
+  }
+}
+
+ */
