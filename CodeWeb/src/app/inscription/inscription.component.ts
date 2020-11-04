3f70730cd32af1b92e82373fd3f07648827c7841
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-11-04 21:28:28 +0100

    inscription refonctionnel

--- CodeWeb/src/app/inscription/inscription.component.ts
+++ CodeWeb/src/app/inscription/inscription.component.ts
@@ -19,10 +19,17 @@ export class InscriptionComponent implements OnInit {
 
   submit(data) {
     this.http.post('http://62.210.130.145:3000/inscription', data)
-      .subscribe((result) =>
-        console.warn('result', result)
+      .subscribe((res) =>
+        {
+            if (data.formInscriptionMdp === data.formInscriptionMdpConfirmation) {
+              alert('cool');
+            }
+            else{
+              alert('pas cool');
+            }
+          }
       );
+    console.log(data)
     alert('inscription completer');
-    location.reload();
   }
 }
