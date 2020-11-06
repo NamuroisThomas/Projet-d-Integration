b80a974662e5154bcfe73b80ea5c818ae6d9342d
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-11-07 00:34:25 +0100

    début détail des demandes

--- CodeWeb/src/app/app-routing.module.ts
+++ CodeWeb/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import {EditProfilComponent} from './edit-profil/edit-profil.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {ContactComponent} from './contact/contact.component';
 import {ListeDemandeComponent} from './liste-demande/liste-demande.component';
+import {DetailDemandeComponent} from './detail-demande/detail-demande.component';
 
 
 const appRoutes: Routes = [
@@ -18,6 +19,7 @@ const appRoutes: Routes = [
     children: [
       {path: 'home', component: HomeComponent},  /*remplacer HomeComponent ici par EditProfilComponent pour le moment pour eviter le dédoublement*/
       {path: 'listeAide', component: ListeDemandeComponent},
+      {path: 'listeAide/:idDemande', component: DetailDemandeComponent},
       {path: 'demandeAide', canActivate: [AuthGuard], component: DemandeAideComponent},
       {path: 'inscription', component: InscriptionComponent},
       {path: 'contactPage', component: ContactComponent},
--- CodeWeb/src/app/liste-demande/liste-demande.service.ts
+++ CodeWeb/src/app/liste-demande/liste-demande.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -7,11 +9,30 @@ import { HttpClient } from '@angular/common/http';
 
 export class GetListeDemandeService {
 
+  demandeDetail: any;
   constructor(
     private http: HttpClient
   ){}
 
+  detail(id: number){
+    this.http.get('http://62.210.130.145:3000/demandes?idDemande=' + id).subscribe((res) => {
+      this.demandeDetail = res[Object.keys(res)[2]][0];
+      console.log(this.demandeDetail);
+    });
+    return this.demandeDetail;
+  };
+
+
   listeDemandeCall(){
     return this.http.get('http://62.210.130.145:3000/demandes');
   }
+
+/*  this.api.listeDemandeCall().subscribe((res) => {
+  console.log(res[Object.keys(res)[2]]);
+  this.listeDemande = res[Object.keys(res)[2]];
+  for (const liste of this.listeDemande){
+  console.log(liste);
+}
+console.log(this.listeDemande);
+});*/
 }
