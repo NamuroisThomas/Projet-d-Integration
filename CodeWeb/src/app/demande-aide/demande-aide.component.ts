a3b35dd22a0680d913120749d1765cf72e908fa2
GGEphec <he201732@students.ephec.be>
2020-11-22 20:29:17 +0100

    demande aide hauteur cases
    augmentation del la hauteur des menus déroulant

--- CodeWeb/src/app/demande-aide/demande-aide.component.ts
+++ CodeWeb/src/app/demande-aide/demande-aide.component.ts
@@ -13,61 +13,38 @@ import {GetListeDemandeService} from '../liste-demande/liste-demande.service';
 })
 export class DemandeAideComponent implements OnInit {
   
-  public demandeAide: FormGroup;
+  public demandeAide;
   public errorMessage: string;
   role: any;
   listeCategorie: any;
   listeCodePostaux: any;
-  levelNum: number;
-  booleanNum: number;
-  levels: [
-    {num: 0, name: 'oui'},
-    {num: 1, name: 'non'}
-    ];
   ONDefraiement: any;
+  formDemandeDefraiement: any;
+  formDemandeIdCodePostal: any;
+  formDemandeIdCategorie: any;
   
-  constructor(private formBuilder: FormBuilder,
-              private messageService: MessageService,
-              private router: Router,
-              private http: HttpClient,
-              private api: GetListeDemandeService
-  ) { }
+  constructor( private http: HttpClient,
+              private api: GetListeDemandeService ) { }
   
   ngOnInit(){
     this.role = JSON.parse(localStorage.getItem('user'));
     this.ONDefraiement = [{val: 1, desc: "Oui"}, {val: 0, desc: "Non"}];
     this.api.listeCategorieCall().subscribe((res) => {
-      //console.log(res[Object.keys(res)[2]]);
+      console.log(res[Object.keys(res)[2]]);
       this.listeCategorie = res[Object.keys(res)[2]];
     });
     this.api.listeCodePostauxCall().subscribe(res => {this.listeCodePostaux = res[Object.keys(res)[2]];});
     console.log(this.listeCategorie);
   }
   
-  
-  // onSubmit() {
-  //   const titre =  this.demandeAide.get('titre').value;
-  //   const description = this.demandeAide.get('description').value;
-  //
-  //   console.log(titre, description);
-  // }
-  toNumber(){
-    this.levelNum = +this.levelNum;
-    this.booleanNum = +this.booleanNum;
-    console.log(this.levelNum);
-    console.log(this.booleanNum);
-  }
-  
-  
+
   submit(data){
-    //console.log(form.value.formDemandeCategorie);
-    console.log(data);
     this.http.post('http://62.210.130.145:3000/demande', data)
       .subscribe((res) =>
         console.warn('result', res)
       );
-    // alert('Demande introduite');
-    // location.reload();
+     alert('Demande introduite');
+     location.reload();
   }
 }
 
