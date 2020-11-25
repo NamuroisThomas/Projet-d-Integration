f70583cb4a34854afca56885c18d7808daeef124
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-11-25 10:40:09 +0100

    liste demande accepté

--- CodeWeb/src/app/liste-demande/liste-demande.component.ts
+++ CodeWeb/src/app/liste-demande/liste-demande.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GetListeDemandeService } from './liste-demande.service';
 import {HttpClient} from '@angular/common/http';
+import {error} from 'selenium-webdriver';
+import {isNull} from "util";
 
 @Component({
   selector: 'app-liste-demande',
@@ -18,20 +20,30 @@ export class ListeDemandeComponent implements OnInit {
   ordreDesc: any;
   fa: any;
   fb: any;
+  profil: any;
 
   constructor(private api: GetListeDemandeService,
               private http: HttpClient) { }
 
   detail(id: number){
-    return this.http.get('http://62.210.130.145:3000/demandes?idDemande=' + id).subscribe((res) => {
-      console.log(res[Object.keys(res)[2]][0]);
+    return this.http
+      .get('http://62.210.130.145:3000/demandes?idDemande=' + id)
+      .subscribe((res) => {
       this.demande = res[Object.keys(res)[2]][0];
     });
+  }
+  accepterDemande(data){
+    alert(data);
+  }
+  submit(data){
+    this.http.post('http://62.210.130.145:3000/accepteDemande', data)
+      .subscribe((res) =>
+        console.warn('result', res)
+      );
   }
 
   ngOnInit(){
     this.api.listeDemandeCall().subscribe((res) => {
-      console.log(res[Object.keys(res)[2]]);
       this.listeDemande = res[Object.keys(res)[2]];
       this.listeDemande1 = this.listeDemande.slice();
       this.listeDemande2 = this.listeDemande.slice();
@@ -45,8 +57,6 @@ export class ListeDemandeComponent implements OnInit {
         this.fb = new Date(b.dateDemande);
         return this.fb -  this.fa;
       });
-      console.log(this.listeDemande);
-      console.log(this.ordreDesc);
     });
     this.filtre = 'all';
   }
