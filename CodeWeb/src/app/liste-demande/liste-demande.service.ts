1cbdc80a1a2035cd2fd885454400254f0394a39d
BrianLuk10 <56024639+BrianLuk10@users.noreply.github.com>
2020-11-29 15:58:44 +0100

    pagination + update bootstrap

--- CodeWeb/src/app/liste-demande/liste-demande.service.ts
+++ CodeWeb/src/app/liste-demande/liste-demande.service.ts
@@ -34,6 +34,57 @@ export class GetListeDemandeService {
     return this.http.get('http://62.210.130.145:3000/demandes');
   }
 
+  listeDemandeDefraimentCall(){
+    return this.http.get('http://62.210.130.145:3000/demandeDefraiement');
+  }
+
+  listeDemandeAllCall(){
+    return this.http.get('http://62.210.130.145:3000/demandeNonAcceptee');
+  }
+
+  listeDemandeNonCall(){
+    return this.http.get('http://62.210.130.145:3000/demandeAcceptee');
+  }
+
+  listeDemandeCourseCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=1');
+  }
+
+  listeDemandeAideMenCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=2');
+  }
+
+  listeDemandeTravauxCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=3');
+  }
+
+  listeDemandeTransportCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=4');
+  }
+
+  listeDemandeCoutureCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=5');
+  }
+
+  listeDemandeJardinageCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=6');
+  }
+
+  listeDemandeElecCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=7');
+  }
+
+  listeDemandePeintureCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=8');
+  }
+
+  listeDemandeCuisineCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=9');
+  }
+
+  listeDemandeAutreCall(){
+    return this.http.get('http://62.210.130.145:3000/demandes?idCateg=24');
+  }
 /*  this.api.listeDemandeCall().subscribe((res) => {
   console.log(res[Object.keys(res)[2]]);
   this.listeDemande = res[Object.keys(res)[2]];
