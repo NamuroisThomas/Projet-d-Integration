d142b6800cce25cc87a3d9a5be7954ff3b50f236
Magnes Simon <55548308+MagnesSimon@users.noreply.github.com>
2020-12-02 16:19:23 +0100

    Bouton retour to Home

--- applicationMobile/app/src/main/java/com/example/needhelp/vue/EnCoursActivity.java
+++ applicationMobile/app/src/main/java/com/example/needhelp/vue/EnCoursActivity.java
@@ -1,4 +1,42 @@
 package com.example.needhelp.vue;
 
-public class EnCoursActivity {
+import android.os.Bundle;
+
+import android.content.Intent;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.needhelp.R;
+import com.example.needhelp.controleur.Controle;
+import com.example.needhelp.modele.Demande;
+
+import java.util.ArrayList;
+
+import com.example.needhelp.R;
+import com.example.needhelp.controleur.Controle;
+
+public class EnCoursActivity extends AppCompatActivity{
+
+    private Controle controle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.en_cours);
+    }
+
+    private void ecouteRetour(){
+        ((Button)findViewById(R.id.deconnexion)).setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EnCoursActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
